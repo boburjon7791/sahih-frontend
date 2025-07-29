855711bc83e4721fa38b31c94f69bd87b7933add
Boburjon <soliyevboburjon95@gmail.com>
2025-07-29 23:37:05 +0500

    Updates

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-// API endpoints
-export const API_BASE = 'https://example.com/api';
-export const CATEGORY_API = `${API_BASE}/categories`;
-export const SERVICES_API = `${API_BASE}/services`;
-export const SERVICE_DETAIL_API = `${API_BASE}/service`;
